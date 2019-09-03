aafcfb9543ba525d0834c4a9537b4853e1875db3
SergeyLykhachov <lykhachovs@gmail.com>
2019-09-03 21:29:31 +0300

    Adds Project Lombok.

--- src/main/java/com/slykhachov/recipeproject/domain/Notes.java
+++ src/main/java/com/slykhachov/recipeproject/domain/Notes.java
@@ -1,7 +1,18 @@
 package com.slykhachov.recipeproject.domain;
 
-import javax.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+import javax.persistence.Entity;
+import javax.persistence.ExcludeDefaultListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.OneToOne;
+
+@Data
+@EqualsAndHashCode(exclude = {"recipe"})
 @Entity
 public class Notes {
     @Id
@@ -13,27 +24,4 @@ public class Notes {
     @Lob
     String recipeNotes;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Recipe getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(Recipe recipe) {
-        this.recipe = recipe;
-    }
-
-    public String getRecipeNotes() {
-        return recipeNotes;
-    }
-
-    public void setRecipeNotes(String recipeNotes) {
-        this.recipeNotes = recipeNotes;
-    }
 }
--- src/main/java/com/slykhachov/recipeproject/services/impl/RecipeServiceImpl.java
+++ src/main/java/com/slykhachov/recipeproject/services/impl/RecipeServiceImpl.java
@@ -3,12 +3,14 @@ package com.slykhachov.recipeproject.services.impl;
 import com.slykhachov.recipeproject.domain.Recipe;
 import com.slykhachov.recipeproject.repositories.RecipeRepository;
 import com.slykhachov.recipeproject.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -19,6 +21,7 @@ public class RecipeServiceImpl implements RecipeService {
     }
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("DEBUG MESSAGE");
         return StreamSupport.stream(recipeRepository.findAll().spliterator(), false)
                 .collect(Collectors.toSet());
     }
