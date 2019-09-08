e434378f4a6cfaf3c84207c1536c9a9164496c82
SergeyLykhachov <lykhachovs@gmail.com>
2019-09-09 00:53:41 +0300

    Adds New Recipe Creation Ability.

--- src/main/java/com/slykhachov/recipeproject/controllers/RecipeController.java
+++ src/main/java/com/slykhachov/recipeproject/controllers/RecipeController.java
@@ -1,11 +1,14 @@
 package com.slykhachov.recipeproject.controllers;
 
+import com.slykhachov.recipeproject.commands.RecipeCommand;
 import lombok.RequiredArgsConstructor;
 
 import com.slykhachov.recipeproject.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequiredArgsConstructor
@@ -14,12 +17,26 @@ public class RecipeController {
 
     private final RecipeService recipeService;
 
-    @RequestMapping("/recipe/show/{id}")
+    @RequestMapping("/recipe/{id}/show")
     public String showById(@PathVariable String id, Model model) {
 
         model.addAttribute("recipe", recipeService.findById(new Long(id)));
 
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/" + savedCommand.getId() + "/show";
     }
 
 }
