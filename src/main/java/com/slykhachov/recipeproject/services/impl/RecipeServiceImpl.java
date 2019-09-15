694f308daabf7d5fdf39c2901ba44d2506dcab71
SergeyLykhachov <lykhachovs@gmail.com>
2019-09-15 23:52:27 +0300

    Adds an error page with an appropriate error message for an invalid input on recipe end point.

--- src/main/java/com/slykhachov/recipeproject/services/impl/RecipeServiceImpl.java
+++ src/main/java/com/slykhachov/recipeproject/services/impl/RecipeServiceImpl.java
@@ -44,8 +44,7 @@ public class RecipeServiceImpl implements RecipeService {
         Optional<Recipe> recipeOptional = recipeRepository.findById(id);
 
         if (!recipeOptional.isPresent()) {
-            //throw new RuntimeException("Recipe Not Found!");
-            throw new NotFoundException("Recipe not found");
+            throw new NotFoundException("Recipe not found for recipe with ID: " + id.toString());
         }
         return recipeOptional.get();
     }
